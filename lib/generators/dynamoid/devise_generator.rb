fcfcbe3802bafa8a85c431150cee39b5b801f1a3
Penguin <say8425@gmail.com>
2020-07-17 16:29:58 +0900

    fix: correct generating type
    this code was mongoid type syntax
    https://docs.mongodb.com/mongoid/current/tutorials/mongoid-documents/#fields

--- lib/generators/dynamoid/devise_generator.rb
+++ lib/generators/dynamoid/devise_generator.rb
@@ -23,33 +23,36 @@ module Dynamoid
       def migration_data
         <<~RUBY
           ## Database authenticatable
-          field :email,              type: String, default: ""
-          field :encrypted_password, type: String, default: ""
+          field :email,              :string, default: ''
+          field :encrypted_password, :string, default: ''
 
           ## Recoverable
-          field :reset_password_token,   type: String
-          field :reset_password_sent_at, type: Time
+          field :reset_password_token,   :string
+          field :reset_password_sent_at, :datetime
 
           ## Rememberable
-          field :remember_created_at, type: Time
+          field :remember_created_at, :datetime
 
           ## Trackable
-          field :sign_in_count,      type: Integer, default: 0
-          field :current_sign_in_at, type: Time
-          field :last_sign_in_at,    type: Time
-          field :current_sign_in_ip, type: String
-          field :last_sign_in_ip,    type: String
+          field :sign_in_count,      :number, default: 0
+          field :current_sign_in_at, :datetime
+          field :last_sign_in_at,    :datetime
+          field :current_sign_in_ip, :string
+          field :last_sign_in_ip,    :string
+        
+          field :created_at, :datetime
+          field :updated_at, :datetime
 
           ## Confirmable
-          # field :confirmation_token,   type: String
-          # field :confirmed_at,         type: Time
-          # field :confirmation_sent_at, type: Time
-          # field :unconfirmed_email,    type: String # Only if using reconfirmable
+          # field :confirmation_token,   :string
+          # field :confirmed_at,         :datetime
+          # field :confirmation_sent_at, :datetime
+          # field :unconfirmed_email,    :string # Only if using reconfirmable
 
           ## Lockable
-          # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
-          # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
-          # field :locked_at,       type: Time
+          # field :failed_attempts, Integer, default: 0 # Only if lock strategy is :failed_attempts
+          # field :unlock_token,    :string # Only if unlock strategy is :email or :both
+          # field :locked_at,       :datetime
         RUBY
       end
     end
